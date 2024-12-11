6413560a2a9b0dd8bbfcfb16ac24dcf0da32af37
Egídio Neto <egidioneto.alves@gmail.com>
2024-12-11 18:18:38 -0300

    primeira implementação para ESP32C3

--- include/DCP.h
+++ include/DCP.h
@@ -1,16 +1,20 @@
 #ifndef __DCP__
 #define __DCP__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
 
 enum e_Flags {
-    FLAG_Instant        = 0u,
-    FLAG_Assynchronous  = 1u
+    FLAG_Instant        = 0b0,
+    FLAG_Assynchronous  = 0b1
 };
 
-typedef struct{
+typedef struct DCP_MODE{
     uint8_t addr;
     union{
         enum e_Flags e_flags;
@@ -20,17 +24,44 @@ typedef struct{
     enum {SLOW = 0, FAST1, FAST2, ULTRA} speed;
 }DCP_MODE;
 
-bool DCPInit(int busPin, DCP_MODE mode);
+bool DCPInit(const unsigned int busPin, const DCP_MODE mode);
 
-typedef struct{
-    enum DCP_Message_type_e {MESSAGE_SYNC = 0u, MESSAGE_L3, MESSAGE_GENERIC} type;
-    size_t size;
-    uint8_t* payload;
-}DCP_Message_t;
+struct DCP_Message_L3_t{
+    uint8_t SOH;        //header
+    uint8_t IDS;        //source ID
+    uint8_t IDD;        //destinaiton ID
+    uint8_t COD;        //instruction code
+    uint8_t data[6];    //data
+    uint8_t PAD;        //padding
+    uint8_t CRC;        //CRC
+};
 
-bool SendMessage(const DCP_Message_t message, const uint8_t addr);
-uint8_t ReadByte();
-DCP_Message_t* ReadMessage();
+struct DCP_Message_Generic_t{
+    uint8_t addr;
+    uint8_t payload[];
+};
+
+struct DCP_Message_t {
+    uint8_t type;
+    union {
+        struct DCP_Message_L3_t L3;
+        struct DCP_Message_Generic_t generic;
+    };
+};
+
+//this is done so we can access the message as a continuous byte array
+//in this way, it is possible to send everything in one go
+typedef union {
+    struct DCP_Message_t * const message;
+    uint8_t * data;
+} DCP_Data_t;
+
+bool SendMessage(const DCP_Data_t message);
+struct DCP_Message_t* ReadMessage();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
 
